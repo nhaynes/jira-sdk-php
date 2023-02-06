68926c3d772de798d3154cf6d67912434a60d4b0
Nick Haynes <nhaynes@adaction.com>
2023-02-06 15:25:58 -0600

    test: added current user test

---
+++ tests/CurrentUserTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Jira SDK PHP project.
+ *
+ * (c) Nick Haynes (https://github.com/nhaynes)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace JiraSdk\Tests;
+
+use JiraSdk\Api\Model\CreateProjectDetails;
+use JiraSdk\Api\Model\User;
+
+class CurrentUserTest extends IntegrationTestCase
+{
+    /**
+     * @covers \JiraSdk\Api\Client::getCurrentUser
+    */
+    public function testGetAllProjects()
+    {
+        $client = $this->createClient();
+
+        $user = $client->getCurrentUser();
+
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertIsString($user->getAccountId());
+    }
+}
