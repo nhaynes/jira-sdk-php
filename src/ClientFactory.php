d4d0576a12d774befcf8200a1771d3e056b2d69d
Nick Haynes <nhaynes@adaction.com>
2023-02-06 15:25:33 -0600

    refactor: updated Client plugins

--- src/ClientFactory.php
+++ src/ClientFactory.php
@@ -14,25 +14,27 @@ declare(strict_types=1);
 namespace JiraSdk;
 
 use Http\Client\Common\Plugin\AuthenticationPlugin;
+use Http\Client\Common\Plugin\BaseUriPlugin;
 use Http\Client\Common\Plugin\ErrorPlugin;
 use Http\Client\Common\PluginClient;
 use Http\Discovery\Psr17FactoryDiscovery;
 use Http\Discovery\Psr18ClientDiscovery;
-use Http\Message\Authentication\Bearer;
+use Http\Message\Authentication\BasicAuth;
 use Psr\Http\Client\ClientInterface;
 
 class ClientFactory
 {
-    public static function create(string $sitename, string $token, ClientInterface $httpClient = null): Client
+    public static function create(string $sitename, string $username, string $token, ClientInterface $httpClient = null): Client
     {
         if (null === $httpClient) {
             $httpClient = Psr18ClientDiscovery::find();
         }
 
         $uri = Psr17FactoryDiscovery::findUriFactory()->createUri("https://{$sitename}.atlassian.net");
-        $authentication = new Bearer($token);
+        $authentication = new BasicAuth($username, $token);
         $pluginClient = new PluginClient($httpClient, [
             new ErrorPlugin(),
+            new BaseUriPlugin($uri, ['replace' => true]),
             new AuthenticationPlugin($authentication),
         ]);
 
